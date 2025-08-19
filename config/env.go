330f8d7e36ead9fe2106d4246d96c7165e8bbfea
olemart1n <olesnoen@gmail.com>
2025-08-19 16:02:49 +0200

    should not log fatal when no env file

--- config/env.go
+++ config/env.go
@@ -22,14 +22,14 @@ type EnvConfig struct {
 func LoadEnvConfig() EnvConfig {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("ℹ️ No .env file found, relying on environment variables")
 	}
 
 	envs := EnvConfig{
 		StorageRegion:           os.Getenv("BUNNY_STORAGE_REGION"),
 		StorageZone:             os.Getenv("BUNNY_STORAGE_ZONE"),
 		StoragePassword:         os.Getenv("BUNNY_STORAGE_PASSWORD"),
-		StoragePasswordReadOnly: os.Getenv("BUNNY_STORAGE_PASSWWORD_READ_ONLY"),
+		StoragePasswordReadOnly: os.Getenv("BUNNY_STORAGE_PASSWORD_READ_ONLY"),
 		PullZone:                os.Getenv("BUNNY_PULL_ZONE"),
 		DatabaseURL:             os.Getenv("DATABASE_URL"),
 		RedisURL:                os.Getenv("REDIS_URL"),
